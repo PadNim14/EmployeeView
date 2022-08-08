181d73d992383b6a75aa19a17175fa9563c50ed5
PadNim14 <PadNim14@github.com>
2022-08-08 10:56:01 -0700

    Initial commit

---
+++ src/main/java/com/wffullstack/wffullstack/WffullstackApplication.java
@@ -0,0 +1,13 @@
+package com.wffullstack.wffullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WffullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WffullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wffullstack/wffullstack/WffullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wffullstack.wffullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WffullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
